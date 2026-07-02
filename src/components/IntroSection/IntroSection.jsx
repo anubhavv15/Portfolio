f3c1949cf2e4d5805bbf71d239fadb41c5f12550
Anubhav chaudhary <164517614+anubhavv15@users.noreply.github.com>
2026-07-02 22:55:22 +0530

    Update CV download link and filename

--- src/components/IntroSection/IntroSection.jsx
+++ src/components/IntroSection/IntroSection.jsx
@@ -35,8 +35,8 @@ const IntroSection = () => {
             As a passionate web developer, I am deeply committed to creating digital solutions that are both functional and user-friendly. My focus is on crafting seamless, intuitive web experiences that allow users to interact effortlessly with the interface. I prioritize designing with the user in mind, ensuring that every project is tailored to meet their needs and expectations. Whether it's building a responsive website or developing a complex web application, my goal is to deliver high-quality, user-driven digital experiences that are smooth, engaging, and easy to navigate....
           </p>
           <a
-            href="/Anubhavcv (2).pdf"
-            download="Anubhavcv.pdf"
+            href="/Anubhav_cv.pdf"
+            download="Anubhav_cv.pdf"
             className="cv-button inline-block bg-white text-indigo-600 px-6 py-3 rounded-xl shadow-md transform transition-all duration-300 ease-in-out"
           >
             📄 Download CV
